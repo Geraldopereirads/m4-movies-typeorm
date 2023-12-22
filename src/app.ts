09ec6b5267c76c2a606042b762ec603e9e42ab75
Geraldo Pereira <110185110+Geraldopereirads@users.noreply.github.com>
2023-12-21 22:19:27 -0300

    feat: add documentation swagger

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import "express-async-errors";
 import express, { Application } from "express";
 import { movieRouter } from "./routers/movie.router";
 import { handleError } from "./middlewares/handleError.middleware";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const app: Application = express();
 app.use(express.json());
 
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/movies", movieRouter);
 app.use(handleError);
 export default app;
--- src/migrations/1703186449087-InitialMigration.ts
+++ src/migrations/1703186449087-InitialMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigration1701401940619 implements MigrationInterface {
-    name = 'InitialMigration1701401940619'
+export class InitialMigration1703186449087 implements MigrationInterface {
+    name = 'InitialMigration1703186449087'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "movies" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "description" text, "duration" integer NOT NULL, "price" integer NOT NULL, CONSTRAINT "UQ_3a794f850bd3e432c46b3faa913" UNIQUE ("name"), CONSTRAINT "PK_c5b2c134e871bfd1c2fe7cc3705" PRIMARY KEY ("id"))`);
