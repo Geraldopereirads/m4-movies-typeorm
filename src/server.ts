3b16be7fe411277a40442f290202196f036acfab
Geraldopereirads <geraldo.pereirads@hotmail.com>
2025-05-06 01:13:47 -0300

    feat: update server.ts and packge.json

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
+import axios from "axios";
 import app from "./app";
 import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize()
-  .then( async () => {
-    console.log("Database connected.")
-    const PORT: number = Number(process.env.PORT || 3000);
+  .then((): void => {
+    console.log("Server is running");
+
+    const PORT: number = Number(process.env.PORT) || 3000;
 
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
+      console.log("Servidor executando");
+
+      setInterval(async () => {
+        try {
+          const url = "https://kimoveis-mow4.onrender.com";
+          const res = await axios.get(url);
+          console.log(`Auto-ping OK: ${res.status}`);
+        } catch (error: any) {
+          console.error("Erro no auto-ping:", error.message);
+        }
+      }, 5 * 60 * 1000);
     });
   })
-  .catch((error) => console.error(error));
+  .catch((err: unknown): void => {
+    console.error("Error during Data Source initialization", err);
+  });
